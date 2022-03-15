266f00653a99cb5160bff6fe39c835779fd77f4a
santanaG <gs8777@gmail.com>
2022-03-15 06:01:38 -0400

    latest

--- public/main.js
+++ public/main.js
@@ -1,135 +1,151 @@
-const buildBullet = index => {
-  const classes = ['bullet', 'bullet-fill', 'bullet-check']
-  const [bullet, fill, check] = classes.map(value => {
-    const element = document.createElement('div')
+if (!window.ApplePaySession) Object.assign(
+  document.querySelector('input#pmap'),
+  { disabled: true }
+)
 
-    element.setAttribute('class', value)
-
-    return element
-  })
-
-  if (index === 0) gsap.set(fill, { width: '100%' })
-
-  { [fill, check].forEach(element => bullet.appendChild(element)) }
-
-  const bulletWrapper = document.querySelector('.slide-bullet-wrapper')
-  bulletWrapper.appendChild(bullet)
-}
+// const preventDefaultSubmit = el => el.addEventListener('submit', e => e.preventDefault())
 
 const { gsap } = window
 
-const setupItem = (item, index) => {
-  gsap.set(item, { xPercent: 100 })
-  Object.assign(item.style, { display: 'flex' })
+// const buildBullet = index => {
+//   const classes = ['bullet', 'bullet-fill', 'bullet-check']
+//   const [bullet, fill, check] = classes.map(value => {
+//     const element = document.createElement('div')
 
-  if (index === 0) gsap.to(item, 0.8, { xPercent: 0 })
-}
+//     element.setAttribute('class', value)
 
-const bulletBack = index => {
-  const fill = `.bullet:nth-child(${index + 1}) .bullet-fill`
-  gsap.set(fill, { width: '0%' })
+//     return element
+//   })
 
-  const previousBullet = `.bullet:nth-child(${index})`
-  gsap.to(previousBullet, { background: 'rgba(0,0,0,0.5)' })
+//   if (index === 0) gsap.set(fill, { width: '100%' })
 
-  const previousCheck = `${previousBullet} .bullet-check`
-  const onComplete = () => gsap.set(previousCheck, { width: '0%' })
+//   { [fill, check].forEach(element => bullet.appendChild(element)) }
 
-  const previousFill = `${previousBullet} .bullet-fill`
-  gsap.to(previousFill, 0.6, { width: '100%', onComplete })
-}
+//   const bulletWrapper = document.querySelector('.slide-bullet-wrapper')
+//   bulletWrapper.appendChild(bullet)
+// }
 
-const bulletNext = index => {
-  const bullet = `.bullet:nth-child(${index + 1})`
-  const check = `${bullet} .bullet-check`
+// const setupItem = (item, index) => {
+//   gsap.set(item, { xPercent: 100 })
+//   Object.assign(item.style, { display: 'flex' })
 
-  gsap.set(check, { width: '100%' })
-  gsap.set(bullet, { background: 'transparent' })
+//   if (index === 0) gsap.to(item, 0.8, { xPercent: 0 })
+// }
 
-  const nextFill = `.bullet:nth-child(${index + 2}) .bullet-fill`
-  const onComplete = () => gsap.set(nextFill, { width: '100%' })
+const initItem = item => gsap.set(item, { xPercent: 100 })
+const resetItem = item => gsap.to(item, 0.8, { xPercent: 100 })
+const displayFlex = item => Object.assign(item.style, { display: 'flex' })
+const showItem = item => gsap.to(item, 0.8, { xPercent: 0 })
+const hideItem = item => gsap.to(item, 0.8, { xPercent: -100 })
 
-  const fill = `${bullet} .bullet-fill`
-  gsap.fromTo(fill, 0.6, { width: '100%' }, { width: '0%', onComplete })
-}
+const updateCollectJS = formData => window.CollectJS.configure({
 
-const slideBack = (item, index) => {
-  gsap.to(item, 0.8, { xPercent: 100 })
+  price: formData[2].value,
 
-  const previousItem = `.slider-item:nth-child(${index})`
-  gsap.to(previousItem, 0.8, { xPercent: 0 })
-}
-
-const slideNext = (item, index) => {
-  gsap.to(item, 0.8, { xPercent: -100 })
-
-  const nextItem = `.slider-item:nth-child(${index + 2})`
-  gsap.to(nextItem, 0.8, { xPercent: 0 })
-}
-
-const updateCollectJS = (order, person, price) => {
-  const { CollectJS } = window
-
-  CollectJS.configure({
-    price,
-    fields: {
-      applePay: {
-        totalLabel: `Custom Payment for Order/PO: ${order}`
-      }
-    },
-    callback: ({ token, tokenType }) => {
-      const inputAttributes = [
-        ['merchant_defined_field_1', order], ['amount', price],
-        ['tokenType', tokenType], ['merchant_defined_field_2', person],
-        ['payment_token', token]
-      ]
-      const form = document.getElementById('finalForm')
-
-      inputAttributes.forEach(([name, value]) => {
-        const element = document.createElement('input')
-        const input = Object.assign(element, { name, value })
-
-        form.appendChild(input)
-      })
-
-      form.submit()
+  fields: {
+    applePay: {
+      totalLabel: `Custom Payment for Order/PO: ${formData[0].value}`
     }
-  })
+  },
+
+  callback: ({ token, tokenType }) => {
+    const form = document.getElementById('formSubmit')
+
+    { [
+      ...formData,
+      { name: 'token', value: token },
+      { name: 'tokenType', value: tokenType }
+    ].forEach(inputValues => {
+      form.appendChild(Object.assign(
+        document.createElement('input'),
+        inputValues
+      ))
+    }) }
+
+    console.log([
+      ...formData,
+      { name: 'token', value: token },
+      { name: 'tokenType', value: tokenType }
+    ])
+
+    form.submit()
+  }
+})
+
+const pay = (gpSlide, orderInfo) => {
+  gpSlide.querySelector('button.back')
+    .addEventListener('click', () => {
+      resetItem(gpSlide)
+      showItem(orderInfo)
+    })
+  const data = [...orderInfo.querySelectorAll('input:required')]
+
+  updateCollectJS(data.map(({ name, value }) => ({ name, value })))
+  showItem(gpSlide)
 }
+
+const setWorkflow = (method, orderInfo) => method
+  .querySelector('button.next')
+  .addEventListener('click', () => {
+    hideItem(method)
+    showItem(orderInfo)
+  })
+
+// const bulletBack = index => {
+//   const fill = `.bullet:nth-child(${index + 1}) .bullet-fill`
+//   gsap.set(fill, { width: '0%' })
+
+//   const previousBullet = `.bullet:nth-child(${index})`
+//   gsap.to(previousBullet, { background: 'rgba(0,0,0,0.5)' })
+
+//   const previousCheck = `${previousBullet} .bullet-check`
+//   const onComplete = () => gsap.set(previousCheck, { width: '0%' })
+
+//   const previousFill = `${previousBullet} .bullet-fill`
+//   gsap.to(previousFill, 0.6, { width: '100%', onComplete })
+// }
+
+// const bulletNext = index => {
+//   const bullet = `.bullet:nth-child(${index + 1})`
+//   const check = `${bullet} .bullet-check`
+
+//   gsap.set(check, { width: '100%' })
+//   gsap.set(bullet, { background: 'transparent' })
+
+//   const nextFill = `.bullet:nth-child(${index + 2}) .bullet-fill`
+//   const onComplete = () => gsap.set(nextFill, { width: '100%' })
+
+//   const fill = `${bullet} .bullet-fill`
+//   gsap.fromTo(fill, 0.6, { width: '100%' }, { width: '0%', onComplete })
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('form')
-    .forEach(form => form.addEventListener('submit', e => e.preventDefault()))
+  const sliderItems = document.querySelectorAll('.slider-item')
+  sliderItems.forEach(item => {
+    initItem(item)
+    displayFlex(item)
+  })
 
-  const items = document.querySelectorAll('.slider-item')
-  items.forEach((item, index, items) => {
-    buildBullet(index)
-    setupItem(item, index)
+  const [method] = sliderItems
+  showItem(method)
 
-    const back = item.querySelector('button.back')
-    if (back) back.addEventListener('click', () => {
-      bulletBack(index)
-      slideBack(item, index)
-    })
+  const orderInfo = document.getElementById('orderInfo')
+  setWorkflow(method, orderInfo)
 
-    const next = item.querySelector('button.next')
-    if (next) next.addEventListener('click', () => {
-      const areFilled = [...item.querySelectorAll('[required]')]
-        .map(({ value }) => value)
-        .every(Boolean)
+  orderInfo.querySelector('button.back').addEventListener('click', () => {
+    resetItem(orderInfo)
+    showItem(method)
+  })
 
-      if (areFilled) {
-        const nextIsLast = index === items.length - 2
-        if (nextIsLast) {
-          const allRequired = document.querySelectorAll('[required]')
-          const allValues = [...allRequired].map(({ value }) => value)
+  orderInfo.querySelector('button.next').addEventListener('click', e => {
+    const isValidOrder = [...orderInfo.querySelectorAll('input:required')]
+      .every(e => e.reportValidity())
 
-          updateCollectJS(...allValues)
-        }
-
-        bulletNext(index)
-        slideNext(item, index)
-      }
-    })
+    if (isValidOrder) {
+      hideItem(orderInfo)
+      const { value } = method.querySelector('input:checked')
+      const nextSlide = document.querySelector(`#${value}-pay`)
+      pay(nextSlide, orderInfo)
+    }
   })
 })
